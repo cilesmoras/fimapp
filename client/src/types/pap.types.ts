1fadc1b2f6bd958b894b4d1175f7aeebca3c3c9c
cilesmoras <cilesmoras@gmail.com>
2024-05-10 20:59:18 +0800

    refactor: updated the pap types

--- client/src/types/pap.types.ts
+++ client/src/types/pap.types.ts
@@ -1,11 +1,6 @@
-import { z } from "zod";
-
-export const papSchema = z.object({
-  code: z.string().min(4).max(45),
-  name: z.string().min(1).max(250),
-  description: z.string().max(250).optional(),
-});
-
-export type Pap = z.infer<typeof papSchema> & {
+export type Pap = {
   id?: number;
+  code: string;
+  name: string;
+  description?: string;
 };
