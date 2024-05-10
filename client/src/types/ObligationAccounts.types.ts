c3cb1e9e44d8244067720c8637c275c98b9ebc3e
cilesmoras <cilesmoras@gmail.com>
2024-05-10 16:57:53 +0800

    feat(types): created ObligationRequest and ObligationAccounts types

---
+++ client/src/types/ObligationAccounts.types.ts
@@ -0,0 +1,6 @@
+export type ObligationAccounts = {
+  id?: number;
+  mfo_paps_id: number;
+  chart_of_accounts_id: number;
+  amount: number;
+};
---
+++ client/src/types/ObligationRequest.types.ts
@@ -0,0 +1,10 @@
+export type ObligationRequest = {
+  id?: number;
+  serial_no: string;
+  fund_cluster: string;
+  payee: string;
+  payee_office: string;
+  payee_office_address: string;
+  particulars: string;
+  date: Date;
+};
