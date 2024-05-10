78943672793fe122729f485151314709e6e58621
cilesmoras <cilesmoras@gmail.com>
2024-05-10 17:08:58 +0800

    wip: obligation request and status

--- client/src/pages/obligation_requests/ObligationRequests.form.tsx
+++ client/src/pages/obligation_requests/ObligationRequests.form.tsx
@@ -1,12 +1,14 @@
-import { CheckIcon } from "@heroicons/react/20/solid";
+import { ObligationRequest } from "@customTypes/ObligationRequest.types";
+import { CheckIcon } from "@heroicons/react/16/solid";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Button from "@ui/Button";
-import Panel from "@ui/Panel";
 import SpinnerIcon from "@ui/SpinnerIcon";
 import TextInput from "@ui/TextInput";
+import Textarea from "@ui/Textarea";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
-import ObligationAccountsTable from "./ObligationAccounts.table";
+import Panel from "./../../components/ui/Panel";
+import ObligationAccountsTable from "./components/ObligationAccounts.table";
 
 const chartOfAccountsSchema = z.object({
   id: z.number(),
@@ -16,13 +18,12 @@ const chartOfAccountsSchema = z.object({
 });
 
 const obligationAccountsSchema = z.object({
-  // id: z.number(),
   mfo_paps_id: z.coerce.number(),
   chart_of_accounts_id: chartOfAccountsSchema,
   amount: z.coerce.number({ invalid_type_error: "Amount must be a number" }),
 });
 
-export type ObligationAccounts = {
+export type ObligationAccountsFormValues = {
   obligation_accounts: z.infer<typeof obligationAccountsSchema>[];
 };
 
@@ -37,7 +38,9 @@ const obligationRequestSchema = z.object({
   obligation_accounts: z.array(obligationAccountsSchema).nonempty(),
 });
 
-export type ObligationRequest = z.infer<typeof obligationRequestSchema>;
+export type ObligationRequestFormValues = z.infer<
+  typeof obligationRequestSchema
+>;
 
 type ObligationRequestProps = {
   data?: ObligationRequest;
@@ -52,7 +55,7 @@ export default function ObligationRequestsForm({
     control,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<ObligationRequest>({
+  } = useForm<ObligationRequestFormValues>({
     resolver: zodResolver(obligationRequestSchema),
   });
 
@@ -60,8 +63,6 @@ export default function ObligationRequestsForm({
     control,
     name: "obligation_accounts",
   });
-
-  console.log(errors);
 
   const serialNoError = errors.serial_no;
   const fundClusterError = errors.fund_cluster;
@@ -71,147 +72,230 @@ export default function ObligationRequestsForm({
   const payeeOfficeAddressError = errors.payee_office_address;
   const dateError = errors.date;
 
-  async function onSubmit(data: ObligationRequest) {
+  async function onSubmit(data: ObligationRequestFormValues) {
     console.log(data);
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Panel width="full">
-          <div className="">
-            <h1 className="font-semibold text-gray-900 text-base mb-4">
-              {isAddMode ? "Add new ORS" : "Edit ORS"}
-            </h1>
-            <div className="flex flex-col gap-y-8 sm:flex-row sm:gap-x-14">
-              <div className="basis-1/2 space-y-4">
-                <Controller
-                  name="serial_no"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      label="Serial no."
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
-                      variant={serialNoError && "danger"}
-                      helpText={serialNoError?.message}
+      <Panel width="full">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-6 sm:space-y-8">
+            <div>
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                {isAddMode ? "Create new ORS" : "Edit ORS"}
+              </h2>
+
+              <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="date"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Date
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Controller
+                      name="date"
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                          id="date"
+                          type="date"
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ""}
+                          variant={dateError && "danger"}
+                          helpText={dateError?.message}
+                        />
+                      )}
                     />
-                  )}
-                />
-                <Controller
-                  name="fund_cluster"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      label="Fund cluster"
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
-                      variant={fundClusterError && "danger"}
-                      helpText={fundClusterError?.message}
+                  </div>
+                </div>
+
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="serial_no"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Serial no.
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Controller
+                      name="serial_no"
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                          id="serial_no"
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ""}
+                          variant={serialNoError && "danger"}
+                          helpText={serialNoError?.message}
+                        />
+                      )}
                     />
-                  )}
-                />
-                <Controller
-                  name="particulars"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      label="Particulars"
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
+                  </div>
+                </div>
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="fund_cluster"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Fund cluster
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Controller
+                      name="fund_cluster"
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                          id="fund_cluster"
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ""}
+                          variant={fundClusterError && "danger"}
+                          helpText={fundClusterError?.message}
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="particulars"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Particulars
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Textarea
+                      id="particulars"
+                      name="particulars"
+                      control={control}
                       variant={particularsError && "danger"}
                       helpText={particularsError?.message}
                     />
-                  )}
-                />
-                <Controller
-                  name="date"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      type="date"
-                      label="Date"
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
-                      variant={dateError && "danger"}
-                      helpText={dateError?.message}
-                    />
-                  )}
-                />
-              </div>
-              <div className="basis-1/2 space-y-4">
-                <h2 className="text-base leading-7 text-gray-900">
-                  Payee information
-                </h2>
-                <Controller
-                  name="payee"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      label="Name"
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
-                      variant={payeeError && "danger"}
-                      helpText={payeeError?.message}
-                    />
-                  )}
-                />
-                <Controller
-                  name="payee_office"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      label="Office"
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
-                      variant={payeeOfficeError && "danger"}
-                      helpText={payeeOfficeError?.message}
-                    />
-                  )}
-                />
-                <Controller
-                  name="payee_office_address"
-                  control={control}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      label="Office address"
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value || ""}
-                      variant={payeeOfficeAddressError && "danger"}
-                      helpText={payeeOfficeAddressError?.message}
-                    />
-                  )}
-                />
+                  </div>
+                </div>
               </div>
             </div>
-            <ObligationAccountsTable
-              control={control}
-              fieldsArray={fieldsArray}
-            />
+
+            <div>
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Payee Information
+              </h2>
+              <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+                Please enter the details of the payee
+              </p>
+
+              <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="payee"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Name
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Controller
+                      name="payee"
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                          id="payee"
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ""}
+                          variant={payeeError && "danger"}
+                          helpText={payeeError?.message}
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="office"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Office
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Controller
+                      name="payee_office"
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                          id="office"
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ""}
+                          variant={payeeOfficeError && "danger"}
+                          helpText={payeeOfficeError?.message}
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-4">
+                  <label
+                    htmlFor="office_address"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Office address
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0 lg:w-1/2">
+                    <Controller
+                      name="payee_office_address"
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                          id="office_address"
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ""}
+                          variant={payeeOfficeAddressError && "danger"}
+                          helpText={payeeOfficeAddressError?.message}
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Obligations
+              </h2>
+              <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+                Please enter atleast one
+              </p>
+              <ObligationAccountsTable
+                control={control}
+                fieldsArray={fieldsArray}
+              />
+            </div>
           </div>
-        </Panel>
-        <div className="sm:flex sm:justify-end sm:gap-4 mt-4">
-          <Button type="submit" size="xl" disabled={isSubmitting}>
-            {isSubmitting ? (
-              <>
-                <SpinnerIcon /> Saving...
-              </>
-            ) : (
-              <>
-                <CheckIcon className="-ml-0.5 size-5" aria-hidden="true" /> Save
-                ORS
-              </>
-            )}
-          </Button>
-        </div>
-      </form>
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <div className="sm:flex sm:justify-end sm:gap-4 mt-4">
+              <Button type="submit" size="lg" disabled={isSubmitting}>
+                {isSubmitting ? (
+                  <>
+                    <SpinnerIcon /> Saving...
+                  </>
+                ) : (
+                  <>
+                    <CheckIcon className="-ml-0.5 size-5" aria-hidden="true" />{" "}
+                    Save ORS
+                  </>
+                )}
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Panel>
     </>
   );
 }
---
+++ client/src/pages/obligation_requests/components/AccountsEntryRow.tsx
@@ -0,0 +1,83 @@
+import {
+  ComboboxDropdownItem,
+  SelectOptionProps,
+} from "@customTypes/uiComponents.types";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import ComboboxDropdown from "@ui/ComboboxDropdown";
+import Select from "@ui/Select";
+import TextInput from "@ui/TextInput";
+import {
+  Control,
+  Controller,
+  FieldArrayWithId,
+  UseFieldArrayRemove,
+} from "react-hook-form";
+import {
+  ObligationAccountsFormValues,
+  ObligationRequestFormValues,
+} from "../ObligationRequests.form";
+
+type AccountsEntryRowProps = {
+  pap: SelectOptionProps[];
+  chartOfAccounts: ComboboxDropdownItem[];
+  control: Control<ObligationRequestFormValues>;
+  field: FieldArrayWithId<ObligationAccountsFormValues>;
+  index: number;
+  remove: UseFieldArrayRemove | undefined;
+};
+
+export default function AccountsEntryRow({
+  pap,
+  chartOfAccounts,
+  control,
+  field,
+  index,
+  remove,
+}: AccountsEntryRowProps) {
+  return (
+    <tr key={field.id}>
+      <td className="whitespace-nowrap px-2 py-2">
+        <ComboboxDropdown<ObligationRequestFormValues>
+          items={chartOfAccounts}
+          name={`obligation_accounts.${index}.chart_of_accounts_id`}
+          control={control}
+        />
+      </td>
+      <td className="whitespace-nowrap px-2 py-2">
+        <Controller
+          name={`obligation_accounts.${index}.mfo_paps_id`}
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Select
+              options={pap}
+              value={value}
+              onChange={onChange}
+              onBlur={onBlur}
+            />
+          )}
+        />
+      </td>
+      <td className="whitespace-nowrap px-2 py-2">
+        <Controller
+          name={`obligation_accounts.${index}.amount`}
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              type="number"
+              onChange={onChange}
+              onBlur={onBlur}
+              value={value || ""}
+              className="basis-full"
+            />
+          )}
+        />
+      </td>
+      <td className="flex whitespace-nowrap px-2 py-2">
+        <XMarkIcon
+          onClick={() => (remove === undefined ? undefined : remove(index))}
+          className="size-8 mt-2 text-danger-500 p-1 hover:bg-danger-50 cursor-pointer"
+        />
+      </td>
+    </tr>
+  );
+}
