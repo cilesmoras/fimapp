468f500786ffaf1842dff6d8b0f52cdc02aa4ef7
cilesmoras <cilesmoras@gmail.com>
2024-05-11 20:47:56 +0800

    chore: added DataTableColumns type

--- client/src/types/uiComponents.types.ts
+++ client/src/types/uiComponents.types.ts
@@ -14,7 +14,7 @@ export type DataTableColumns = {
   name: string;
   label: string;
   size?: number;
-  alignment?: string;
+  alignment?: "right" | "left";
   isNumber?: boolean;
   isTruncated?: boolean;
   content?: (item: { id: number; name: string }) => ReactNode;
