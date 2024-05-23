71336de41da1cd573103bffe98b3f4429f59f2c4
cilesmoras <cilesmoras@gmail.com>
2024-05-23 15:59:58 +0800

    feat: redirect when clicking to obligation request card

--- client/src/pages/obligation_requests/components/ObligationRequests.card.tsx
+++ client/src/pages/obligation_requests/components/ObligationRequests.card.tsx
@@ -1,4 +1,5 @@
 import { ViewObligationRequest } from "@customTypes/ObligationRequest.types";
+import { Link } from "react-router-dom";
 
 type ObligationRequestCardProps = {
   obligationRequest: ViewObligationRequest;
@@ -17,32 +18,35 @@ export default function ObligationRequestCard({
     total_amount,
   } = obligationRequest;
   return (
-    <li
-      key={id}
-      className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow hover:shadow-lg hover:cursor-pointer"
-    >
-      <div className="flex w-full items-center justify-between space-x-6 p-6">
-        <div className="flex-1 truncate">
-          <div className="flex items-center space-x-3">
-            <h3 className="truncate text-sm text-gray-900">{serial_no}</h3>
-            <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-              {new Date(date).toDateString()}
-            </span>
+    <Link to={`/obligation-requests/${id}/edit`}>
+      <li
+        key={id}
+        className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow hover:shadow-lg hover:cursor-pointer"
+      >
+        <div className="flex w-full items-center justify-between space-x-6 p-6">
+          <div className="flex-1 truncate">
+            <div className="flex items-center space-x-3">
+              <h3 className="truncate text-sm text-gray-900">{serial_no}</h3>
+              <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                {new Date(date).toDateString()}
+              </span>
+            </div>
+            <p className="mt-1 truncate text-gray-900 font-medium">{payee}</p>
+            <p className="mt-1 truncate text-sm text-gray-500">
+              {payee_office}
+            </p>
+            <p className="mt-1 truncate text-sm text-gray-500">{particulars}</p>
+            <p className="mt-1 truncate text-gray-900 font-medium">
+              ₱{Number(total_amount)?.toLocaleString() ?? 0}
+            </p>
           </div>
-          <p className="mt-1 truncate text-gray-900 font-medium">{payee}</p>
-          <p className="mt-1 truncate text-sm text-gray-500">{payee_office}</p>
-          <p className="mt-1 truncate text-sm text-gray-500">{particulars}</p>
-          <p className="mt-1 truncate text-gray-900 font-medium">
-            ₱{Number(total_amount)?.toLocaleString() ?? 0}
-          </p>
-        </div>
-        {/* <img
+          {/* <img
           className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
           src={person.imageUrl}
           alt=""
         /> */}
-      </div>
-      {/* <div>
+        </div>
+        {/* <div>
         <div className="-mt-px flex divide-x divide-gray-200">
           <div className="flex w-0 flex-1">
             <a
@@ -67,6 +71,7 @@ export default function ObligationRequestCard({
           </div>
         </div>
       </div> */}
-    </li>
+      </li>
+    </Link>
   );
 }
